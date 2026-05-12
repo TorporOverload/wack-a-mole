da1124e87160d63cadae61a6b80d45f0ab1c528a
Ahmed Shameel <62275846+TorporOverload@users.noreply.github.com>
2026-05-12 17:39:58 +0500

    Refine hammer positioning and transform behavior

--- scripts.js
+++ scripts.js
@@ -8,17 +8,17 @@ $(window).on("mousemove", function (e) {
   // use "e.originalEvent.offsetX" and "e.originalEvent.offsetY" for mouse position
   // using this alone will put the element on the bottom right so u might need to + 100 or - 100
   const hammer = $(".hammer");
-  hammer.css("left", e.pageX - 20);
-  hammer.css("top", e.pageY + 15);
+  hammer.css("left", e.pageX);
+  hammer.css("top", e.pageY);
 });
 
 $(window).on("click", function (e) {
   // rotate the hammer and hit the mole
   // using setTimeout, rotate it back 100ms later
   const hammer = $(".hammer");
-  hammer.css("transform", "rotate(-45deg)");
+  hammer.css("transform", "translate(-20px, -30px) rotate(-45deg)");
   setTimeout(() => {
-    hammer.css("transform", "rotate(0deg)");
+    hammer.css("transform", "translate(-20px, -30px) rotate(0deg)");
   }, 100);
 });
 
